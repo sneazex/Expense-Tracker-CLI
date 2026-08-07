d614e15132b9391f94187bb95617c3b5567519ad
Woidexc <granit.ege@proton.me>
2026-08-07 12:57:52 +0300

    Revert "Merge pull request #5 from sneazex/revert-3-feature/storage-tests"
    This reverts commit 5fdb62cba1030c21beb6e23c3d15f1413ee0c258, reversing
    changes made to a38d6c737a7eeb0b1ad5c589dd1eef61dab0d0f2.

---
+++ test/test_storage.py
@@ -0,0 +1,29 @@
+import os
+import pytest
+from src.expense_tracker.storage import load_expenses, save_expenses
+
+# temporary file path
+TEST_FILE = os.path.join("data", "test_expenses.json")
+
+@pytest.fixture(autouse=True)
+def cleanup():
+    #Cleanup before and after each test
+    if os.path.exists(TEST_FILE):
+        os.remove(TEST_FILE)
+    yield
+    if os.path.exists(TEST_FILE):
+        os.remove(TEST_FILE)
+
+def test_load_non_existent_file():
+    # Test loading from a non-existent file
+    result = load_expenses(TEST_FILE)
+    assert result == []
+
+def test_save_and_load_expenses():
+    # Test saving and loading expenses
+    sample_data = [{"id": 1, "amount": 12.5, "category": "food", "note": "lunch"}]
+    save_expenses(sample_data, filepath = TEST_FILE)
+    assert os.path.exists(TEST_FILE) # Check if file was created
+
+    loaded_data = load_expenses(TEST_FILE)
+    assert loaded_data == sample_data #Check if loaded data matches saved data
