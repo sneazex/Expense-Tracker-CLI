1b251a1d93aa57032594c26996a31f0aee294a23
Sneaze <paragamerz753@gmail.com>
2026-08-07 11:36:39 +0700

    feature/project-stucture, cli, models, test-models (#4)
    * feat: add Expense dataclass
    
    * feat: implement Expense to_dict and from_dict
    
    * feat: complete Expense model
    
    * test: add unit test for Expense model
    
    * feat: implement command-line interface
    
    * feat: add application entry point
    
    ---------
    
    Co-authored-by: Sneaze <pargamerz753@gmail.com>

---
+++ src/expense_tracker/models.py
@@ -0,0 +1,43 @@
+from dataclasses import dataclass
+from datetime import date
+from decimal import Decimal
+from typing import Any, Self
+from uuid import UUID
+
+
+@dataclass
+class Expense:
+    id: UUID
+    date: date
+    amount: Decimal
+    category: str
+    note: str | None = None
+
+    def __post_init__(self):
+        if not isinstance(self.date, date):
+            raise TypeError("A valid calendar date is required")
+
+        if self.amount == 0:
+            raise ValueError("Expense amount cannot be zero")
+
+        if not self.category or not self.category.strip():
+            raise ValueError("Category cannot be empty")
+
+    def to_dict(self) -> dict[str, Any]:
+        return {
+            "id": str(self.id),
+            "date": self.date.isoformat(),
+            "amount": str(self.amount),
+            "category": self.category,
+            "note": self.note
+        }
+
+    @classmethod
+    def from_dict(cls, data: dict[str, Any]) -> Self:
+        return cls(
+            id=UUID(data["id"]),
+            date=date.fromisoformat(data["date"]),
+            amount=Decimal(data["amount"]),
+            category=data["category"],
+            note=data.get("note")
+        )
